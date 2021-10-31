f7a80f7682d7849d008887b7c5a0355402ba86f7
Harsha69 <62492737+upadrastaharshavardhan@users.noreply.github.com>
2021-10-31 22:06:03 +0530

    Add files via upload

---
+++ src/linkedlist/SinglyLinkedListNode.java
@@ -0,0 +1,10 @@
+package linkedlist;
+
+public class SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode next;
+
+    SinglyLinkedListNode(int data) {
+        this.data = data;
+    }
+}
